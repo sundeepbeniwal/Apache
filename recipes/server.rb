05e63d7d00feb107314881dd831e6704cdbd18f8
vagrant <vagrant@localhost.localdomain>
2018-08-01 21:51:09 +0000

    added system info using node attributes to index.html file

--- recipes/server.rb
+++ recipes/server.rb
@@ -9,7 +9,10 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
-  content '<h1>Hello, World!</h1>'
+  content "<h1>Hello, World!</h1>
+HOSTNAME: #{node['hostname']}
+IPADDRESS: #{node['ipaddress']}
+"
 end
 
 service 'httpd' do
