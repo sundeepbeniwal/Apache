48fb45de29c4078c99147170e8ef19519cc3e931
vagrant <vagrant@localhost.localdomain>
2018-08-02 03:42:34 +0000

    replaced index.html file reource with a template index.html.erb resource

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,11 +8,8 @@ package 'httpd' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello, World!</h1>
-HOSTNAME: #{node['hostname']}
-IPADDRESS: #{node['ipaddress']}
-"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
 
 service 'httpd' do
